de0fb9d2f2d258af6107de023fe4bbb8162e7888
aja531 <aja531@gmail.com>
2020-08-13 15:24:07 -0600

    Update yamlConversion.py
    Removed code that was looping over multiple .yml files.  Now the module will only read files that are passed to it, and return the proper values from the function.

--- api/yamlConversion.py
+++ api/yamlConversion.py
@@ -1,44 +1,36 @@
-# Read all yaml files in specified directory.
+# Read specified yaml file.
 # Find the appliance name in the file, and place into the "name" and "tag" variables.
 # Take YAML configuration info and convert to base64 encoding.
-# Upload preconfiguration to Orchestrator.
+# Return "name", "tag", and "data" from function.
 
-def readYAML(orchIP, loginCookie, path):
-    import os
+def readYAML(filename):
     import base64
     import yaml
-    from .preconfigOrch import preconfigUpload
 
-    for entry in os.listdir(path):
-        if entry.endswith('.yml'):
-          filename = os.path.join(path, entry)
-          with open(filename, 'r') as file:
-              dict = yaml.load(file, Loader=yaml.FullLoader)
+    with open(filename, 'r') as file:
+        dict = yaml.load(file, Loader=yaml.FullLoader)
 
-# place hostname into "name" and "tag", to be used in API post later
+# place hostname into "name" and "tag"
 
-          name = (dict['applianceInfo']['hostname'])
-          tag = (dict['applianceInfo']['hostname'])
-          file.close()
+        name = (dict['applianceInfo']['hostname'])
+        tag = (dict['applianceInfo']['hostname'])
+        file.close()
 
 # open and read yaml file as text into a string, so it can be converted to base64 encoding
 
-          f = open(filename, "r")
-          text = f.read()
+        f = open(filename, "r")
+        text = f.read()
 
 # convert the string obatained from the yaml file to base64 encoding
 
 
-          conversion = base64.b64encode(text.encode("utf-8"))
+        conversion = base64.b64encode(text.encode("utf-8"))
 
 # decode base64 from bytes to string, to get rid of the " 'b' characters"
 
-          data = conversion.decode()
+        data = conversion.decode()
 
-# Pass data to preconfiguration module for upload via API
-
-          upload = preconfigUpload(orchIP, loginCookie, name, tag, data)
-
+        return name, tag, data
 
 ### For local testing ###
 
