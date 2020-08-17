31496e19335f5ca09eddb9ea160441a107789c35
aja531 <aja531@gmail.com>
2020-08-16 21:05:28 -0600

    Update preconfigOrch.py
    Added user feedback

--- api/preconfigOrch.py
+++ api/preconfigOrch.py
@@ -20,6 +20,10 @@ def preconfigUpload(orchIP, loginCookie, name, tag, data):
     payload = "{{\r\n  \"name\": \"{0}\",\r\n  \"serialNum\": null,\r\n  \"tag\": \"{1}\",\r\n  \"comment\": null,\r\n  \"autoApply\": true,\r\n  \"configData\": \"{2}\"\r\n}}".format(name, tag, data)
     headers = {'Content-Type': 'application/json'}
     response = requests.request("POST", url, headers=headers, data=payload, cookies=loginCookie, verify=False)
-    print(response.text.encode('utf8'))
+
+    if response.status_code == 200:
+        print(name + ' preconfiguration successfully uploaded to Orchestrator.')
+    else:
+        sys.exit('Failed to upload ' + name + ' preconfiuration to Orchestrator.')
 
 # end
