ee5d2c44ff56488a305a03cf6ff010deb69fbe83
Jason Anderson <anderson.jason573@gmail.com>
2020-09-20 10:52:10 -0600

    Update __init__.py
    Updated comments

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,8 @@
 """
 ***************************************************************
-
 This module initializes the 'api' package, when it is imported.
-It imports all of the functions from the modules in the api 
-package, so they can be called by using 'api.<function>'.
-
+It imports all of the classes and methods from the modules in 
+the api package, so they can be called.
 ***************************************************************
 """
 
